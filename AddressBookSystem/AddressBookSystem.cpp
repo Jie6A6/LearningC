f541302c35d14b82d363dc9c8ccf03e963e84c33
Jie6A6 <Jie6A6@gmail.com>
2026-01-03 12:41:50 +0800

    2026.01.03

--- AddressBookSystem/AddressBookSystem.cpp
+++ AddressBookSystem/AddressBookSystem.cpp
@@ -93,6 +93,31 @@ void addPerson(Addressbooks * abs)
     
 }
 
+// 2. 显示所有联系人
+void showPerson(Addressbooks * abs)
+{
+    // 判断通讯录中人数是否为 0 ，如果为 0 ，提示记录为空；如果不为 0 ，显示记录的联系人信息
+    if (abs->m_Size == 0)
+    {
+        cout << "当前记录为空" << endl;
+    }
+    else
+    {
+        for (int i = 0; i < abs->m_Size; i++)
+        {
+            cout << "姓名：" << abs->personArray[i].m_Name << "\t"; // 制表符操作
+            cout << "性别：" << (abs->personArray[i].m_Gender == 1 ? "男" : "女") << "\t";
+            cout << "年龄：" << abs->personArray[i].m_Age<< "\t";
+            cout << "电话：" << abs->personArray[i].m_Phone << "\t";
+            cout << "住址：" << abs->personArray[i].m_Addr << endl;
+        }
+    }
+    system("pause");
+    system("cls");
+}
+
+
+
 // 菜单界面
 void showMenu()
 {
@@ -130,10 +155,9 @@ int main()
         {
         case 1: // 1. 添加联系人
             addPerson(&abs); // 利用地址传递，可以修饰实参
-            /* code */
             break;
         case 2: // 2. 显示联系人
-            /* code */
+            showPerson(&abs);
             break;
         case 3: // 3. 删除联系人
             /* code */
